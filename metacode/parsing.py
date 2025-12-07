998fbc95b8ef9c6248879af001716a1dd0fb74e1
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2025-12-07 03:31:31 +0300

    Add TODO

--- metacode/parsing.py
+++ metacode/parsing.py
@@ -73,6 +73,7 @@ def get_candidates(comment: str) -> Generator[ParsedComment, None, None]:
                         arguments.append(argument.id)
                     elif isinstance(argument, Constant):
                         arguments.append(argument.value)
+                    # TODO: delete this branch if minimum supported version of Python is > 3.8 (we have the Index node only in old Pythons).
                     elif isinstance(argument, Index) and isinstance(argument.value, BinOp) and isinstance(argument.value.left, Name) and isinstance(argument.value.right, Name):  # pragma: no cover
                         arguments.append(f'{argument.value.left.id}-{argument.value.right.id}')
                     elif isinstance(argument, BinOp) and isinstance(argument.left, Name) and isinstance(argument.right, Name):
